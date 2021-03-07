e03249a65dbecb34b22c5b341ee7f653ac507e68
panthercoding <79004908+panthercoding@users.noreply.github.com>
2021-03-07 17:01:38 -0600

    Create Day.java

---
+++ Day.java
@@ -0,0 +1,18 @@
+public class Day{
+  
+  public static int dayOfWeek(int m, int d, int y){
+    /* to be implemented */
+  }
+
+  public static void main(String[] args){
+
+    int month = Integer.parseInt(args[0]);
+    int day = Integer.parseInt(args[1]);
+    int year = Integer.parseInt(args[2]);
+
+    int D = dayOfWeek(month, day, year);
+
+    /* start coding the print statments from here */
+
+  }
+}
